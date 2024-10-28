d3fc62acf90de68456e3cba79f1da6738de38efe
PedroCassunde <pedro.lima4@estudante.ifms.edu.br>
2024-10-28 13:22:34 -0400

    birdsofafeather

---
+++ src/pages/Home.jsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+import ListaProdutos from "./ListaProdutos";
+
+export default function Home() {
+
+const [lista, setLista] = useState([]);
+
+    useEffect(() => {
+        const receberListaProdutos = async () => {
+            try {
+                const resposta = await fetch('https://fakestoreapi.com/products');
+                const dados = await resposta.json();
+                setLista(dados);
+            } catch (erro) {
+                alert("A conexão com a API falhou!")
+            }
+        }
+        receberListaProdutos();
+    }, []);
+
+    return (
+        <>
+        <h1>Lista de Produtos</h1>
+        <ListaProdutos lista={produtos}/>
+        </>
+    );}
